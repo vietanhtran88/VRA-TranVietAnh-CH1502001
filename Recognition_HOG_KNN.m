3fc9cfd91571e01e92d84af60017dffddcd73b04
vietanhtran88 <vietanhtran88@gmail.com>
2017-07-05 18:53:42 +0700

    HOG
    Sua HOG

--- Recognition_HOG_KNN.m
+++ Recognition_HOG_KNN.m
@@ -1,18 +1,17 @@
 function Accurate_rate=Recognition_HOG_KNN()  
-%     strFileName = ['MDL\Mdl_HOG_KNN.mat'];
-%     [imgTrainImagesAll,lblTrainLabelsAll]=load_data('Data\train-images.idx3-ubyte','Data\train-labels.idx1-ubyte');
-%     features_data_train=extract_HOG_Features(imgTrainImagesAll);
-%     Mdl_HOG_KNN=fitcknn(features_data_train',lblTrainLabelsAll);
-%     save(strFileName,'Mdl_HOG_KNN');
- 
+    strFileName = ['MDL\Mdl_HOG_KNN.mat'];
+    [imgTrainImagesAll,lblTrainLabelsAll]=load_data('Data\train-images.idx3-ubyte','Data\train-labels.idx1-ubyte');
+    features_data_train=extract_HOG_Features(imgTrainImagesAll);
+    Mdl_HOG_KNN=fitcknn(features_data_train',lblTrainLabelsAll);
+    save(strFileName,'Mdl_HOG_KNN');
 %     load(strFileName);
-%     [imgTestImagesAll,lblTestLabelsAll]=load_data('Data\t10k-images.idx3-ubyte','Data\t10k-labels.idx1-ubyte');
-%      
-%     features_data_test=extract_HOG_Features(imgTestImagesAll);
-%     lblresult=predict(Mdl_HOG_KNN,features_data_test');
-%     nResult=(lblresult==lblTestLabelsAll);
-%     save('Results\nResult_HOG_KNN.mat','nResult');
-    load('Results\nResult_HOG_KNN.mat');
+    [imgTestImagesAll,lblTestLabelsAll]=load_data('Data\t10k-images.idx3-ubyte','Data\t10k-labels.idx1-ubyte');
+     
+    features_data_test=extract_HOG_Features(imgTestImagesAll);
+    lblresult=predict(Mdl_HOG_KNN,features_data_test');
+    nResult=(lblresult==lblTestLabelsAll);
+    save('Results\nResult_HOG_KNN.mat','nResult');
+%     load('Results\nResult_HOG_KNN.mat');
     nCount=sum(nResult);
     Accurate_rate=nCount/100;  
 end
--- Recognition_HOG_svm.m
+++ Recognition_HOG_svm.m
@@ -1,22 +1,21 @@
 function Accurate_rate=Recognition_HOG_svm()
-%     strFileName = ['MDL\Mdl_HOG_SVM.mat'];
-    %load data train
-%     [imgTrainImagesAll,lblTrainLabelsAll]=load_data('Data\train-images.idx3-ubyte','Data\train-labels.idx1-ubyte');
-%     % trich xuat feature cua tung anh
-%     features_data_train=extract_HOG_Features(imgTrainImagesAll);
-%     
-%     %dung model
-%     Mdl_HOG_svm=fitcecoc(features_data_train',lblTrainLabelsAll);
-%     save(strFileName,'Mdl_HOG_svm');
+    strFileName = ['MDL\Mdl_HOG_SVM.mat'];
+    [imgTrainImagesAll,lblTrainLabelsAll]=load_data('Data\train-images.idx3-ubyte','Data\train-labels.idx1-ubyte');
+    % trich xuat feature cua tung anh
+    features_data_train=extract_HOG_Features(imgTrainImagesAll);
+    
+    %dung model
+    Mdl_HOG_svm=fitcecoc(features_data_train',lblTrainLabelsAll);
+    save(strFileName,'Mdl_HOG_svm');
 %     load(strFileName);
     
-    %load data test
-%     [imgTestImagesAll,lblTestLabelsAll]=load_data('Data\t10k-images.idx3-ubyte','Data\t10k-labels.idx1-ubyte');    
-%     features_data_test=extract_HOG_Features(imgTestImagesAll);
-%     lblresult=predict(Mdl_HOG_svm,features_data_test');
-%     nResult=(lblresult==lblTestLabelsAll);
-%     save('Results\nResult_HOG_SVM.mat','nResult');
-    load('Results\nResult_HOG_SVM.mat');
+    load data test
+    [imgTestImagesAll,lblTestLabelsAll]=load_data('Data\t10k-images.idx3-ubyte','Data\t10k-labels.idx1-ubyte');    
+    features_data_test=extract_HOG_Features(imgTestImagesAll);
+    lblresult=predict(Mdl_HOG_svm,features_data_test');
+    nResult=(lblresult==lblTestLabelsAll);
+    save('Results\nResult_HOG_SVM.mat','nResult');
+%     load('Results\nResult_HOG_SVM.mat');
     nCount=sum(nResult);
     Accurate_rate=nCount/100;
 end
