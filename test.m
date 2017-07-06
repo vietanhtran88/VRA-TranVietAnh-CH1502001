c77c5393332af81025a4f7f289727cf4eaf3fd0b
vietanhtran88 <vietanhtran88@gmail.com>
2017-07-07 00:39:56 +0700

    Update 7/7/17

--- test.m
+++ test.m
@@ -15,11 +15,18 @@ function test()
     LBP_KNN=sum(nResult)/100;
     load('Results\nResult_LBP_SVM.mat');
     LBP_SVM=sum(nResult)/100;
-    y=([RAW_KNN RAW_SVM LBP_KNN LBP_SVM]);    
+    load('Results\nResult_HOG_KNN.mat');
+    HOG_KNN=sum(nResult)/100;
+    load('Results\nResult_HOG_SVM.mat');
+    HOG_SVM=sum(nResult)/100;
+    load('Results\confMatrix_BOW.mat');
+    BOW=mean(diag(confMatrix))*100;
+    
+    y=([RAW_KNN RAW_SVM LBP_KNN LBP_SVM HOG_KNN HOG_SVM BOW]);    
     h=bar(y,'FaceColor',[0 .5 .5]);
     yb = cat(1, h.YData);
     xb = bsxfun(@plus, h(1).XData, [h.XOffset]');
     hold on;
     htxt = text(xb(:),yb(:), cellstr(num2str(yb(:))),'HorizontalAlignment','center','VerticalAlignment','bottom');
-    set(gca,'XTickLabel', {'Raw KNN', 'Raw SVM', 'LBP KNN', 'LBP SVM',});
+    set(gca,'XTickLabel', {'Raw KNN', 'Raw SVM', 'LBP KNN', 'LBP SVM','HOG KNN', 'HOG SVM', 'BOW'});
 end
