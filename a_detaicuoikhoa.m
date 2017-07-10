ebc6bc69c92134f694a2ccce612f1dc6907b11a5
vietanhtran88 <vietanhtran88@gmail.com>
2017-07-10 11:53:33 +0700

    update 10/7/17
    update 10/7/17

--- a_detaicuoikhoa.m
+++ a_detaicuoikhoa.m
@@ -1,11 +1,27 @@
 function a_detaicuoikhoa()
+    [imgTrainImagesAll,lblTrainLabelsAll]=load_data('Data\train-images.idx3-ubyte','Data\train-labels.idx1-ubyte');
+    [imgTestImagesAll,lblTestLabelsAll]=load_data('Data\t10k-images.idx3-ubyte','Data\t10k-labels.idx1-ubyte');   
+    load('Results\nResult_RAW_KNN.mat');
+    RAW_KNN=sum(nResult)/100;
+    load('Results\nResult_RAW_SVM.mat');
+    RAW_SVM=sum(nResult)/100;
+    load('Results\nResult_LBP_KNN.mat');
+    LBP_KNN=sum(nResult)/100;
+    load('Results\nResult_LBP_SVM.mat');
+    LBP_SVM=sum(nResult)/100;
+    load('Results\nResult_HOG_KNN.mat');
+    HOG_KNN=sum(nResult)/100;
+    load('Results\nResult_HOG_SVM.mat');
+    HOG_SVM=sum(nResult)/100;
+    load('Results\confMatrix_BOW.mat');
+    BOW=mean(diag(confMatrix))*100;
     
-    y=([Recognition_raw_knn() Recognition_raw_svm() Recognition_HOG_KNN() Recognition_HOG_svm() Recognition_LBP_KNN() Recognition_LBP_svm() Recognition_BoW_svm()]);    
+    y=([RAW_KNN RAW_SVM LBP_KNN LBP_SVM HOG_KNN HOG_SVM BOW]);    
     h=bar(y,'FaceColor',[0 .5 .5]);
     yb = cat(1, h.YData);
     xb = bsxfun(@plus, h(1).XData, [h.XOffset]');
     hold on;
     htxt = text(xb(:),yb(:), cellstr(num2str(yb(:))),'HorizontalAlignment','center','VerticalAlignment','bottom');
-    set(gca,'XTickLabel', {'Raw KNN', 'Raw SVM', 'Hog KNN', 'Hog SVM', 'LBP KNN', 'LBP SVM', 'BoW'});
+    set(gca,'XTickLabel', {'Raw KNN', 'Raw SVM', 'LBP KNN', 'LBP SVM','HOG KNN', 'HOG SVM', 'BOW'});
 
 end
